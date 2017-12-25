9f8cc50eaf9e1b066dee5f0b3194f84245336df7
i5misswrong <1279010287@qq.com>
2017-12-25 10:41:28 +0800

    20171225_i5misswrong
    项目初始化

---
+++ CA/Rule.py
@@ -0,0 +1,24 @@
+
+def PeopleMove(p,direction):
+    if direction==1:
+        p.x=p.x-1
+        p.y=p.y+1
+    elif direction==2:
+        p.y=p.y+1
+    elif direction==3:
+        p.x=p.x+1
+        p.y=p.y+1
+    elif direction==4:
+        p.x=p.x-1
+    elif direction==5:
+        p.x=p.x
+    elif direction==6:
+        p.x=p.x+1
+    elif direction==7:
+        p.x=p.x-1
+        p.y=p.y-1
+    elif direction==8:
+        p.y=p.y-1
+    elif direction==9:
+        p.x=p.x+1
+        p.y=p.y-1
