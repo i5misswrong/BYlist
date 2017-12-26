e24d2a9e90822651eaba061c2efc98c0fdfe042a
notoriousby <34637753+notoriousby@users.noreply.github.com>
2017-12-26 15:19:40 +0800

    20171226

--- CA/InitPeople.py
+++ CA/InitPeople.py
@@ -3,14 +3,14 @@ def creatAppointPeo():
     allPeople=[]
 
     b1=Block.Block(1)
-    b1.x=3
-    b1.y=5
+    b1.x=10
+    b1.y=8
     b1.type=True
     allPeople.append(b1)
 
     b2=Block.Block(1)
-    b2.x=4
-    b2.y=5
+    b2.x=10
+    b2.y=9
     b2.type=False
     allPeople.append(b2)
 
